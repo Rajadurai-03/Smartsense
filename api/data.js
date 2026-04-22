8c225cf9558a4b38454b1d52f409dea064d4c3ac
Rajadurai M <134929471+Rajadurai-03@users.noreply.github.com>
2026-04-22 21:31:34 +0530

    data.js

--- api/data.js
+++ api/data.js
@@ -1,13 +1,9 @@
 import { MongoClient } from "mongodb";
 
+const uri = "mongodb+srv://Raja:Raja%402003@cluster0.rhk0ynu.mongodb.net/iot?retryWrites=true&w=majority";
+
 export default async function handler(req, res) {
   try {
-    const uri = process.env.MONGO_URI;
-
-    if (!uri) {
-      return res.status(500).json({ error: "Missing MONGO_URI" });
-    }
-
     const client = new MongoClient(uri);
     await client.connect();
 
@@ -16,21 +12,15 @@ export default async function handler(req, res) {
 
     if (req.method === "POST") {
       await col.insertOne(req.body);
-      await client.close();
       return res.status(200).json({ msg: "stored" });
     }
 
     if (req.method === "GET") {
       const data = await col.find().sort({ _id: -1 }).limit(1).toArray();
-      await client.close();
       return res.status(200).json(data[0] || {});
     }
 
-    await client.close();
-    res.status(405).json({ error: "Method not allowed" });
-
   } catch (err) {
-    console.error(err);
     res.status(500).json({ error: err.message });
   }
 }
